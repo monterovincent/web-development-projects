a4c03011766ae0310b33f90b8dc3dc3b45445b60
EBUBE VINCENT <124905297+monterovincent@users.noreply.github.com>
2025-06-21 07:28:43 +0100

    Add files via upload

---
+++ Tboss.js
@@ -0,0 +1,65 @@
+<!doctype.html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<script language="javascript" src="vincent.javascript">
+
+
+</script> 
+
+</head>
+<body>
+
+<form name="my form6" >
+	<input type="text" name="name" value="enter your name"</form>
+
+<form name="My form7">
+<table>
+<tr>
+<td><input type="text" name="van" value="enter your first name"></td>
+<td><input type="text" name="vin" value="enter last name"></td>
+</tr>
+<tr>
+<td><input type="text" name="log" value="enter your grade"></td>
+<td><input type="text" name="laf" value="result"></td>
+</tr>
+
+<tr>
+<td><input type="text" name="god" value="enter the new grade"></td>
+<td><input type="text" name="bell" value="result"></td>
+</tr>
+
+<tr>
+<td><input type="text" name="chain" value="enter your first name"></td>
+<td><input type="text" name="jak" value="enter last name"></td>
+</tr>
+
+<tr>
+<td><input type="text" name="fab" value="enter your first name"></td>
+<td><input type="text" name="log" value="enter your last name"></td>
+</tr>
+
+<tr>
+<td><input type="number" name="kel" value="enter a number and press the tab key"></td>
+</tr>
+
+
+<tr>
+<td><input type="number" name="gul" value="enter a number"></td>
+<td><input type="number" name="brl" value="generate even number"></td>
+</tr>
+
+<tr>
+<td><input type="number" name="hat" value="enter a number"></td>
+<td><input type="number" name="lad" value="generate even number"></td>
+</tr>
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ karakuta.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+
+function rich ()
+{
+	var fname = document.d.firstname.value;
+	while (fname.length== 0)
+	{
+		fname = prompt ("enter your first name","");
+	}
+	function mmm()
+	{
+		var fname = document.e.firstname.value;
+		do
+		{
+			fname= prompt ("enter your first name","");
+			document.e.firstname.value= fname;}
+			while (fname.length== 0)
+	}
+	function valid()
+	{
+		var numb1, numb2;
+		numb1 = parseInt (document.f.even.value);
+		numb2 = 1
+		for( numb2; numb2 < numb1; numb2 = numb2 + 2
+		{
+			alert(numb2);
+		}
+	
+	
+
+
+
+
+
+
+
